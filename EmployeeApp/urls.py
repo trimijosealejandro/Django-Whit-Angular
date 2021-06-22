dda38b1856146203d29bf3fcae53e1e3cd257385
trimijosealejandro93 <trimijosealejandro@yahoo.com>
2021-06-22 09:38:55 -0400

    terminando el api rest

--- EmployeeApp/urls.py
+++ EmployeeApp/urls.py
@@ -1,8 +1,16 @@
 from django.conf.urls import url
 from django.conf.urls import url
+from django.conf.urls.static import static
+from django.conf import settings
 from EmployeeApp import views
+
+
 
 urlpatterns=[
     url(r'^department/$', views.departmentApi),
-    url(r'^department/([0-9]+)$', views.departmentApi)
-]
+    url(r'^department/([0-9]+)$', views.departmentApi),
+    url(r'^employee/$', views.employeeApi),
+    url(r'^employee/([0-9]+)$', views.employeeApi),
+
+    url(r'^SaveFile$', views.SaveFile)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- EmployeeApp/views.py
+++ EmployeeApp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from django.http.response import JsonResponse
+from django.core.files.storage import default_storage
 
 from EmployeeApp.models import Departaments, Employees
 from EmployeeApp.serializers import DepartmentSerializer, EmployeeSerializer
@@ -33,4 +34,41 @@ def departmentApi(request,id=0):
         department.delete()
         return JsonResponse("Eliminate Successful!!!", safe=False)
     return JsonResponse("Failed to Eliminate" , safe=False)
+
+    # Create your views here.
+@csrf_exempt
+def employeeApi(request,id=0):
+    if request.method=='GET':
+        employees = Employees.objects.all()
+        employees_serializer = EmployeeSerializer(employees, many=True)
+        return JsonResponse(employees_serializer.data, safe=False)
+    elif request.method=='POST':
+        employee_data = JSONParser().parse(request)
+        employee_serializer = EmployeeSerializer(data=employee_data)
+        if employee_serializer.is_valid():
+            employee_serializer.save()
+            return JsonResponse("Added Successful!!!", safe=False)
+        return JsonResponse("Failed to Add" , safe=False)
+    elif request.method=='PUT':
+        employee_data = JSONParser().parse(request)
+        employee=Employees.objects.get(employeeId=employee_data['employeeId'])
+        employee_serializer = EmployeeSerializer(employee,data=employee_data)
+        if employee_serializer.is_valid():
+            employee_serializer.save()
+            return JsonResponse("Update Successful!!!", safe=False)
+        return JsonResponse("Failed to Update" , safe=False)
+    elif request.method=='DELETE':
+        employee=Employees.objects.get(employeeId=id)
+        employee.delete()
+        return JsonResponse("Eliminate Successful!!!", safe=False)
+    return JsonResponse("Failed to Eliminate" , safe=False)
+
+@csrf_exempt
+def SaveFile(request):
+    file=request.FILES['uploadedFile']
+    file_name = default_storage.save(file.name, file)
+    return JsonResponse(file_name,safe=False)
+
+
+
 
